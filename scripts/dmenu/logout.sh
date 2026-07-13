45cbe8ed4b6b1d557f1641351ccfa9dcaabeae68
Eduardo Cabrera Montero <isc.eduardo.came@gmail.com>
2026-07-13 10:20:40 -0600

    se borro la capeta scripts:

--- scripts/dmenu/logout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dmenu=$(echo -e "APAGAR\n󰑓REINICIAR\n󰍃CERRAR WM"|dmenu  -nb '#000000' -fn 'Hack Nerd Font Mono: size=14' -l 3)
-case "$dmenu" in
-"APAGAR") shutdown now;;
-"󰑓REINICIAR") reboot;;
-"󰍃CERRAR WM") qtile cmd-obj -o root -f shutdown ;;
-
-*) echo  " ";;
-esac
--- scripts/dmenu/screenshoter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dmenu=$(echo -e "SCROT\nSCROT-REGION"|dmenu  -nb '#000000' -fn 'Hack Nerd Font Mono: size=14' -l 3)
-case "$dmenu" in
-"SCROT") scrot;;
-"SCROT-REGION") scrot -s;;
-
-*) echo  " ";;
-esac
